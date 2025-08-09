3108927ca3179d909e15b5fa12ab1f096b9f8ffb
t-hayashi <t.hayashi.dev@gmail.com>
2025-08-09 23:19:11 +0900

    fix dockerfile

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,18 +1,22 @@
 import { SummarizeSettingForm } from "@/components/partials/summertize-setting-form";
 import { Separator } from "@/components/ui/separator";
+import { db } from "@/database";
 import { getUserId } from "@/lib/auth";
 import { ListTimelineArticle } from "@/server/queries/list-timeline-article";
 
 export default async function Home() {
 	const userId = await getUserId();
 	const articles = await ListTimelineArticle({ userId });
+	const setting = await db.query.userSetting.findFirst({
+		where: (userSetting, { eq }) => eq(userSetting.userId, userId),
+	});
 	return (
 		<div>
 			<Section title="Summarize">
 				<SummarizeSettingForm
-					language="English"
-					length="short"
-					customInstructions=""
+					language={setting?.summaryLanguage ?? "english"}
+					length={setting?.summaryLength ?? "short"}
+					customInstructions={setting?.summaryInstructions ?? ""}
 					articles={articles}
 				/>
 			</Section>
