d298b5efd1294e6c53ea05465f7a113940caf6e0
t-hayashi <t.hayashi.dev@gmail.com>
2025-08-09 23:22:55 +0900

    fix redis connection

--- server/queues/crawl-queue.ts
+++ server/queues/crawl-queue.ts
@@ -4,7 +4,6 @@ export const RedisConnection = {
 	host: process.env.REDIS_HOST ?? "localhost",
 	port: process.env.REDIS_PORT ? Number(process.env.REDIS_PORT) : 6379,
 	password: process.env.REDIS_PASSWORD || undefined,
-	tls: process.env.REDIS_HOST?.includes("localhost") ? undefined : {},
 };
 
 // クロールキュー
