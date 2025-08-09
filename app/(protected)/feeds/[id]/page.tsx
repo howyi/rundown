32d9641f932dc713216c849dedb22b4d2795173a
t-hayashi <t.hayashi.dev@gmail.com>
2025-08-10 05:30:19 +0900

    add icons

--- app/(protected)/feeds/[id]/page.tsx
+++ app/(protected)/feeds/[id]/page.tsx
@@ -1,6 +1,20 @@
+import type { Metadata } from "next";
 import { ArticleList } from "@/components/shared/article-list";
 import { getUserId } from "@/lib/auth";
 import { ListFeedArticle } from "@/server/queries/list-feed-article";
+
+export async function generateMetadata({
+	params,
+}: {
+	params: Promise<{ id: string }>;
+}): Promise<Metadata> {
+	const feedId = await params.then((p) => p.id);
+	const userId = await getUserId();
+	const feed = await ListFeedArticle({ userId, feedId });
+	return {
+		title: `rundown | ${feed.title}`,
+	};
+}
 
 export default async function Home({
 	params,
--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default async function Home() {
 	});
 
 	if (session) {
-		redirect("/feeds");
+		redirect("/timeline");
 	}
 
 	return (
